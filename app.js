ff42419e332bcf88ec5ed10895dc6ee6aa8a0150
marialm3 <luciamendozazumaran@gmail.com>
2022-01-20 20:30:40 -0500

    week03 files

---
+++ app.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const PATH = process.env.PORT || 5000;
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+app.listen(PATH);
