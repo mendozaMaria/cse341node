b9ca9cb79df2bf531b338d492d8a6f40da42bd06
marialm3 <luciamendozazumaran@gmail.com>
2022-02-04 21:35:02 -0500

    Files of week05

---
+++ week05/app.js
@@ -0,0 +1,78 @@
+const path = require('path');
+const PATH = process.env.PORT || 5000;
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+const csrf = require('csurf');
+const flash = require('connect-flash');
+
+const errorController = require('./controllers/error');
+const User = require('./models/user');
+
+const MONGODB_URI = 'mongodb://Lucia:Mongocluster@cluster0-shard-00-00.2rtxd.mongodb.net:27017,cluster0-shard-00-01.2rtxd.mongodb.net:27017,cluster0-shard-00-02.2rtxd.mongodb.net:27017/shop?ssl=true&replicaSet=atlas-ytq3a7-shard-0&authSource=admin&retryWrites=true&w=majority'
+
+const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions', //a collection where your session will be stored
+});
+
+const csrfProtection = csrf();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  session({
+    secret: 'my secret', 
+    resave: false, 
+    saveUninitialized: false, 
+    store: store
+  })
+);
+app.use(csrfProtection);
+app.use(flash());
+
+app.use((req, res, next) => {
+  if(!req.session.user){
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+app.use(authRoutes);
+
+app.use(errorController.get404);
+
+mongoose
+  .connect(
+    MONGODB_URI
+  )
+  .then(result => {
+    app.listen(PATH);
+  })
+  .catch(err => {
+    console.log(err);
+  });
